5d4661eb0246ff4d88e9029b825b008b4b961001
Roge55555 <roge55555@gmail.com>
2023-03-22 14:55:09 +0300

    clear import

--- src/main/java/com/effective/shop/entity/Characteristic.java
+++ src/main/java/com/effective/shop/entity/Characteristic.java
@@ -1,13 +1,10 @@
 package com.effective.shop.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 @AllArgsConstructor
--- src/main/java/com/effective/shop/entity/Product.java
+++ src/main/java/com/effective/shop/entity/Product.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @AllArgsConstructor
